dc5a2c57c98b2a4252d114ecbf119cc5853db8e5
0xcacti <0xcacti@gmail.com>
2023-06-06 22:04:07 -0400

    figure out just some basic outline stuff

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
-use clap::Parser;
+use clap::{crate_version, Parser};
 
 const ABOUT: &str = "Get a random verse from the Bible.";
 
 /// bible-rs is a command line tool for getting a random verse from the Bible.
-#[derive(Parser, Debug)]
-#[command(name="bible-rs", version, about, long_about = ABOUT)]
+#[derive(Debug, Parser)]
+#[command(name="bible-rs", version=crate_version!(), about="daily bread", long_about = ABOUT)]
 struct BibleParser {
     /// Desired book of the Bible
-    #[arg(short, long)]
+    #[arg(short = 'b', long = "book")]
     book: String,
 }
 
