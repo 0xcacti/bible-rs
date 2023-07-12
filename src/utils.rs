b58560d3bdd2b2938ad7cb5347d0bd39919f1f33
0xcacti <0xcacti@gmail.com>
2023-07-12 01:18:36 -0400

    catch unwraps in submodules

--- src/utils.rs
+++ src/utils.rs
@@ -1,4 +1,4 @@
-use anyhow::Result;
+use anyhow::{Context, Result};
 use chrono::Local;
 use rand::{rngs::StdRng, Rng};
 use rand_core::SeedableRng;
@@ -32,6 +32,9 @@ pub fn hex_to_u64(b: &[u8]) -> Option<u64> {
 pub fn get_client_and_headers(api_key: &str) -> Result<(Client, HeaderMap)> {
     let client = Client::new();
     let mut headers = HeaderMap::new();
-    headers.insert("api-key", HeaderValue::from_str(api_key).unwrap());
+    headers.insert(
+        "api-key",
+        HeaderValue::from_str(api_key).context("error inserting api-key")?,
+    );
     Ok((client, headers))
 }
